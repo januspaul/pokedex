71fd2cdf2997404d1cc85007bd9767452b686da0
januspaul <januspaul52@gmail.com>
2022-12-21 09:20:17 +0800

    modified to display 4 random pokemons

--- src/Featured.jsx
+++ src/Featured.jsx
@@ -1,30 +1,28 @@
 import React from 'react';
 
-const Featured=()=>{
-    const [pokemon,setPokemon] = React.useState(null);
-    
+const Featured = () => {
+  const [pokemons, setPokemons] = React.useState([]);
 
-    React.useEffect(()=> {
-        const randomId = Math.floor(Math.random() * 807) + 1;
-        fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
-        .then((response)=> response.json())
-        .then((data) => setPokemon(data));
-    },[]);
+  React.useEffect(() => {
+    for (let i = 1; i < 3; i++) {
+      const randomId = Math.floor(Math.random() * 807) + 1;
+      fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+        .then((response) => response.json())
+        .then((data) => setPokemons((prevPokemons) => [...prevPokemons, data]));
+    }
+  }, []);
 
-    return(
-        <div>
-            {
-                pokemon && (
-                    <div>
-                        
-                        <img src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name}/>
-                        <h1>{pokemon.name}</h1>
-                        <p>{pokemon.types[0].type.name}</p>
-                    </div>
-                )
-            }
+  return (
+    <div>
+      {pokemons.map((pokemon) => (
+        <div key={pokemon.id}>
+          <img src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} />
+          <h1>{pokemon.name}</h1>
+          <p>{pokemon.types[0].type.name}</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default Featured;
