eb4c49b9a3f854637cc84a7e24db5f59d39f5c5d
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2022-12-21 13:39:31 +0800

    uppercase 1st letter name of pokemon in featured

--- src/components/home/Featured.jsx
+++ src/components/home/Featured.jsx
@@ -19,7 +19,7 @@ const Featured = () => {
         <Card key={pokemon.id} style={{ width: "200px" }}>
           <Card.Img variant="top" src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} />
           <Card.Body>
-            <Card.Title>{pokemon.name}</Card.Title>
+            <Card.Title>{pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}</Card.Title>
             <Card.Text>{pokemon.types[0].type.name}</Card.Text>
           </Card.Body>
         </Card>
