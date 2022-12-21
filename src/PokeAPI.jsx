b2887e93694dacb6872241b47de241119bea8874
januspaul <januspaul52@gmail.com>
2022-12-21 11:53:53 +0800

    modified pokeapi to get search result from hero

--- src/PokeAPI.jsx
+++ src/PokeAPI.jsx
@@ -1,77 +1,68 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function PokeAPI() {
-  const [name, setname] = useState("");
-  const [Find, setFind] = useState("mewtwo");
+const PokeAPI = ({ search }) => {
   const [pokemon, setPokemon] = useState(null);
 
   useEffect(() => {
     async function getData() {
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${Find}`);
-      setPokemon(response.data);
+      if (search) {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${search}`
+        );
+        setPokemon(response.data);
+      } else {
+        setPokemon(null);
+      }
     }
 
     getData();
-  }, [Find]);
-
-  const Typename = (event) => {
-    setname(event.target.value);
-  };
-
-  const Search = () => {
-    if (name !== "") setFind(name);
-    setname("");
-  };
+  }, [search]);
 
   if (!pokemon) {
-    return <div>Loading...</div>;
+    return <div>Enter a Pokemon name to search for it using the PokeAPI</div>;
   }
 
   return (
-    <>
-      <div className="back">
-        <div className="card">
-          <input type="text" onChange={Typename} value={name} />
-          <button onClick={Search}>Search</button>
-          <img src={pokemon.sprites.front_default} alt="" />
-          <div className="name">{pokemon.name.toUpperCase()}</div>
-          <div className="type">{pokemon.types[0].type.name}</div>
+    <div>
+      <img src={pokemon.sprites.front_default} alt="" />
+      <div className="name">{pokemon.name.toUpperCase()}</div>
+      <div className="type">{pokemon.types[0].type.name}</div>
 
-          <div className="data">
-            <p>ID: {pokemon.id}</p>
-            <p>Height: {pokemon.height}</p>
-            <p>Weight: {pokemon.weight}</p>
-            <p>Base Experience: {pokemon.base_experience}</p>
-            <p>Abilities:</p>
-            <ul>
-              {pokemon.abilities.map((ability) => (
-                <li key={ability.ability.name}>{ability.ability.name}</li>
-              ))}
-            </ul>
-            <p>Types:</p>
-            <ul>
-              {pokemon.types.map((type) => (
-                <li key={type.type.name}>{type.type.name}</li>
-              ))}
-            </ul>
-            <p>Moves:</p>
-            <ul>
-              {pokemon.moves.map((move) => (
-                <li key={move.move.name}>{move.move.name}</li>
-              ))}
-            </ul>
-            <p>Stats:</p>
-            <ul>
-              {pokemon.stats.map((stat) => (
-                <li key={stat.stat.name}>
-                  {stat.stat.name}: {stat.base_stat}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+      <div className="data">
+        <p>ID: {pokemon.id}</p>
+        <p>Height: {pokemon.height}</p>
+        <p>Weight: {pokemon.weight}</p>
+        <p>Base Experience: {pokemon.base_experience}</p>
+        <p>Abilities:</p>
+        <ul>
+          {pokemon.abilities.map((ability) => (
+            <li key={ability.ability.name}>{ability.ability.name}</li>
+          ))}
+        </ul>
+        <p>Types:</p>
+        <ul>
+          {pokemon.types.map((type) => (
+            <li key={type.type.name}>{type.type.name}</li>
+          ))}
+        </ul>
+        <p>Moves:</p>
+        <ul>
+          {pokemon.moves.map((move) => (
+            <li key={move.move.name}>{move.move.name}</li>
+          ))}
+        </ul>
+        <p>Stats:</p>
+        <ul>
+          {pokemon.stats.map((stat) => (
+            <li key={stat.stat.name}>
+              {stat.stat.name}: {stat.base_stat}
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+    </div>
   );
-}
+};
+
+export default PokeAPI;
