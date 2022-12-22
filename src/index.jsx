e757bc82fb4df450065f8950c24f8be5a1028ac3
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2022-12-22 15:37:54 +0800

    strict mode removed

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <BrowserRouter>
       <App>
       <Routes>
@@ -24,6 +24,6 @@ root.render(
       </Routes>
       </App>
     </BrowserRouter>
-  </React.StrictMode>
+  
 );
 
