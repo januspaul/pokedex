d5c57cb5c5e4616232603daa7702d6b2881920aa
januspaul <januspaul52@gmail.com>
2022-12-22 13:41:34 +0800

    added unique keys to cards

--- src/components/home/Featured.jsx
+++ src/components/home/Featured.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Carousel, Card, CardGroup, Container } from 'react-bootstrap';
+import React from "react";
+import { Carousel, Card, CardGroup, Container } from "react-bootstrap";
 
 const Featured = () => {
   const [pokemons, setPokemons] = React.useState([]);
@@ -16,18 +16,20 @@ const Featured = () => {
         const randomId = Math.floor(Math.random() * 807) + 1;
         fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
           .then((response) => response.json())
-          .then((data) => setPokemons((prevPokemons) => [...prevPokemons, data]));
+          .then((data) =>
+            setPokemons((prevPokemons) => [...prevPokemons, data])
+          );
       }
     };
     loadPokemons();
   }, [currentSlide]);
 
   return (
-    <div style={{ backgroundColor: '#6423B5' }} >
+    <div style={{ backgroundColor: "#6423B5" }}>
       <Container>
         <div>
-          <span className='line'></span>
-          <h3 className='text-style'>Featured Pokemon</h3>
+          <span className="line"></span>
+          <h3 className="text-style">Featured Pokemon</h3>
         </div>
 
         <Carousel activeIndex={currentSlide} onSelect={handleSlideChange}>
@@ -35,14 +37,33 @@ const Featured = () => {
             <Carousel.Item key={index}>
               <CardGroup style={{ justifyContent: "space-around" }}>
                 {pokemons.map((pokemon) => (
-                  <div className="row">
+                  <div key={pokemon.id} className="row">
                     <div className="col-5">
-                      <Card key={pokemon.id} style={{ width: "200px" }} className="mb-3">
-                        <Card.Img variant="top" src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} />
+                      <Card
+                        key={pokemon.id}
+                        style={{ width: "200px" }}
+                        className="mb-3"
+                      >
+                        <Card.Img
+                          variant="top"
+                          src={
+                            pokemon.sprites.other["official-artwork"]
+                              .front_default
+                          }
+                          alt={pokemon.name}
+                        />
                         <Card.Body>
-                          <Card.Title>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</Card.Title>
-                          <Card.Text>#{pokemon.id.toString().padStart(3,0)}</Card.Text>
-                          <img src={`https://veekun.com/dex/media/types/en/${pokemon.types[0].type.name}.png`} alt={pokemon.types[0].type.name}/>
+                          <Card.Title>
+                            {pokemon.name.charAt(0).toUpperCase() +
+                              pokemon.name.slice(1)}
+                          </Card.Title>
+                          <Card.Text>
+                            #{pokemon.id.toString().padStart(3, 0)}
+                          </Card.Text>
+                          <img
+                            src={`https://veekun.com/dex/media/types/en/${pokemon.types[0].type.name}.png`}
+                            alt={pokemon.types[0].type.name}
+                          />
                         </Card.Body>
                       </Card>
                     </div>
@@ -54,7 +75,6 @@ const Featured = () => {
         </Carousel>
       </Container>
     </div>
-
   );
 };
 
