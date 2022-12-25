11dbd2bfbc81f7032ab44e43a9be127f7f83982a
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2022-12-25 20:51:22 +0800

    modal data added

--- src/components/reusable/Hero1.jsx
+++ src/components/reusable/Hero1.jsx
@@ -15,12 +15,10 @@ const Hero = () => {
   };
 
   return (
-    <div className="hero1BG d-flex justify-content-center align-items-center wh-100 text-center vh-100" style={{
+    <div className="hero1BG d-flex justify-content-center align-items-center text-center wh-100 vh-100" style={{
       backgroundImage: `url(${process.env.PUBLIC_URL + '/hero1.png'})`,
       backgroundRepeat: 'no-repeat',
       backgroundSize: '100%',
-
-
     }}>
       <Container className='containerHero'>
         <Row>
@@ -55,18 +53,31 @@ const Hero = () => {
                 show={show}
                 onHide={() => setShow(false)}
                 dialogClassName="modal-90w"
-                aria-labelledby="example-custom-modal-styling-title">
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
                 <Modal.Header closeButton>
-                  <Modal.Title>{pokemon.name}</Modal.Title>
+                  <Modal.Title>{pokemon.name.charAt(0).toUpperCase() +
+                    pokemon.name.slice(1)}</Modal.Title>
                 </Modal.Header>
-
-                <p>Type: {pokemon.types[0].type.name}</p>
+                <Modal.Body>
+                    <ul>{pokemon.abilities.map((ability) => (
+                  <li key={ability.ability.name}>{ability.ability.name}</li>
+                ))}</ul>
+                <ul>{pokemon.types.map((type) => (
+                  <li key={type.type.name}>{type.type.name}</li>
+                ))}</ul>
+                </Modal.Body>
+                <Modal.Footer>
+                  {pokemon.id}
+                </Modal.Footer>
+                
+                
               </Modal>
             </Col>
           </Row>
         )}
       </Container>
-    </div>
+    </div >
   );
 };
 
