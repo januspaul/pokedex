1037dcf2046ca30e25f8d108941e242b870ce3d0
shawshawmee <jilcharlz009@gmail.com>
2022-12-27 14:43:05 +0800

    updated news layout

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,12 +1,9 @@
-import { Button } from "react-bootstrap";
-
-
 const News = () => {
     return (
-        <div>
-            <img className="img-fluid d-block w-100" src="hero1.png" alt="" />
-            <div className="bg-primary text-center">
-                <Button>Latest News</Button>
+        <div className="bg-primary">
+            <img className="img-fluid d-block w-100 mb-3 mt-5" src="hero1.png" alt="" />
+            <div className="d-flex justify-content-center text-white">
+                <p className="bg-primary ps-5 pe-5 pb-2 pt-2 fw-bold radius-bottom" style={{ position: "absolute", }}>LATEST NEWS</p>
             </div>
             <img className="img-fluid d-block w-100" src="BGnews 1.png" alt="" />
         </div>
