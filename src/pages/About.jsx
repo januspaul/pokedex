068d0343cd44695551bfa853cbf350ead58ba95b
Darryl Kaye Abella Sanga <117624517+AkosiDK@users.noreply.github.com>
2022-12-28 19:27:30 +0800

    added about page hero

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,20 @@
 const About = () => {
-    return(
-        <div className="m-5 p-5">
-            About Routing
+    return (
+        <div className="contactContainer vw-100 vh-100">
+            <div className="container">
+                <div className="row contactCol1 d-flex align-items-center justify-content-center">
+                    <div className="col-6 text-white">
+                        <h1 className="aboutUsHeader pb-3 text-warning">About us</h1>
+                        <p className="aboutUsDetails">The Pokémon Company International manages the property outside of Asia 
+                            and is responsible for brand management, licensing, marketing, the Pokémon Trading Card Game, 
+                            the animated TV series, home entertainment, and the official Pokémon website.</p>
+
+                    </div>
+                    <div className="col-6">
+                        <img className='contactCol2' src="abouthero1.png" alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
