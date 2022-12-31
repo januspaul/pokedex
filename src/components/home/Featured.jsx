47cac88725a98fff1191463843a096d6f9aec737
AkosiDK <darrylkayeabellasanga@gmail.com>
2022-12-31 10:45:30 +0800

    fixed featured cards

--- src/components/home/Featured.jsx
+++ src/components/home/Featured.jsx
@@ -26,39 +26,40 @@ const Featured = () => {
   }, [currentSlide]);
 
   return (
-    <div style={{ backgroundColor: "#6423B5", backgroundSize:"100%" }} className="featuredContainer p-5">
+    <div style={{ backgroundColor: "#6423B5"}} className="featuredContainer p-5">
       <Container>
         <div>
           <h3 className="text-styleFeatured aboutUsHeader"><span className="lineFeatured"></span>Featured Pokemon</h3>
         </div>
 
-        <Carousel activeIndex={currentSlide} onSelect={handleSlideChange} className="pb-5 mx-5">
+        <Carousel activeIndex={currentSlide} onSelect={handleSlideChange} className="pb-5">
           {[...Array(3)].map((_, index) => (
-            <Carousel.Item key={index}>
+            <Carousel.Item className="" key={index}>
               <CardGroup style={{ justifyContent: "space-around" }}>
                 {pokemons.map((pokemon) => (
                   <div key={pokemon.id} className="row">
-                    <div className="col-5">
+                    <div className="col-12">
                       <Card
                         key={pokemon.id}
-                        style={{ width: "250px" }}
-                        className="mt-1 px-2"
+                        className="featuredBGCards border-0 mt-1 px-2"
                       >
                         <Card.Img
+                          className="pt-3"
                           variant="top"
                           src={
                             pokemon.sprites.other["official-artwork"]
                               .front_default
                           }
                           alt={pokemon.name}
+                          
                         />
-                        <hr />
-                        <Card.Body className="text-center">
-                          <Card.Title className="">
+                     
+                        <Card.Body className="text-center pt-5">
+                          <Card.Title className="text-white">
                             {pokemon.name.charAt(0).toUpperCase() +
                               pokemon.name.slice(1)}
                           </Card.Title>
-                          <Card.Text>
+                          <Card.Text className="text-white">
                             #{pokemon.id.toString().padStart(3, 0)}
                           </Card.Text>
                           <img
