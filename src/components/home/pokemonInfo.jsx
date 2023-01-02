29ff7ba45ed65b5b290c59b827cc5e0d216a2073
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-01-02 17:08:30 +0800

    Added pokemon details

--- src/components/home/pokemonInfo.jsx
+++ src/components/home/pokemonInfo.jsx
@@ -3,25 +3,32 @@ import PokemonFlavorText from './pokemonDescription';
 
 
 function PokemonInfo(props) {
-  const [type, setType] = useState();
+  const [types, setTypes] = useState([]);
   const [id, setID] = useState();
   const [sprites, setSprites] = useState();
   const [pokemonName, setPokemonName] = useState();
-  const [pokemonHeight,setPokemonHeight] = useState();
-  const [pokemonWeight,setPokemonWeight] = useState();
+  const [pokemonHeight, setPokemonHeight] = useState();
+  const [pokemonWeight, setPokemonWeight] = useState();
+  const [stats, setStats] = useState({});
+  const [abilities, setAbilities] = useState([]);
 
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${props.pokemonName}`);
       const data = await response.json();
-      setType(data.types[0].type.name);
+      const statsData = data.stats.reduce((obj, stat) => {
+        obj[stat.stat.name] = stat.base_stat;
+        return obj;
+      }, {});
+      setStats(statsData);
+      setTypes(data.types);
       setID(data.id);
       setPokemonName(data.name);
       setPokemonHeight(data.height);
       setPokemonWeight(data.weight);
-      
       setSprites(data.sprites.other['official-artwork'].front_default);
+      setAbilities(data.abilities);
     }
     fetchData();
   }, [props.pokemonName]);
@@ -30,28 +37,42 @@ function PokemonInfo(props) {
     <div>
       <div className="row">
         <div className="col">
-           #{id}
+          #{id}
         </div>
-        
+
       </div>
       <div className="row">
         <div className="col">
-            <img className='img-fluid d-block' src={sprites} alt={props.pokemonName} />
+          <img className='img-fluid d-block' src={sprites} alt={props.pokemonName} />
         </div>
         <div className="col">
           {props.pokemonName.charAt(0).toUpperCase() + props.pokemonName.slice(1)}
           <PokemonFlavorText textPokemon={pokemonName} />
         </div>
         <div className="row">
-         Type: {type}
+          Type: {types.map(type => (
+          <div key={type.type.name}>{type.type.name}</div>
+        ))}
         </div>
         <div className="row">
-         Height: {pokemonHeight}
+          Height: {pokemonHeight}
         </div>
         <div className="row">
-         Weight: {pokemonWeight}
+          Weight: {pokemonWeight}
         </div>
-      </div>   
+        <div>
+          <h4>Stats:</h4>
+          {Object.keys(stats).map(stat => (
+            <div key={stat}>{stat}: {stats[stat]}</div>
+          ))}
+        </div>
+        <div>
+        <h4>Abilities:</h4>
+        {abilities.map(ability => (
+          <div key={ability.ability.name}>{ability.ability.name}</div>
+        ))}
+      </div>
+      </div>
     </div>
   );
 }
