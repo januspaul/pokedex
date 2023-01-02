9cf6b3f84b256a50a681712b2f899bb6ded556d5
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-01-02 19:48:48 +0800

    ThankYouMessage in Contact Added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,6 @@ import { addDoc, collection, getFirestore } from "firebase/firestore";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-
 const firebaseConfig = {
     apiKey: "AIzaSyA8wNjaBEvU9IE6igfsM8c7wbWd3TKBY6A",
     authDomain: "contactform-pokemon.firebaseapp.com",
@@ -15,26 +14,28 @@ const firebaseConfig = {
     appId: "1:367133485033:web:e625c7c220cf92a5e74d78"
 };
 
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const Contact = () => {
-
     const [name, setName] = useState();
     const [email, setEmail] = useState();
     const [contactnum, setContact] = useState();
     const [feedback, setFeedback] = useState();
+    const [formSubmitted, setFormSubmitted] = useState(false); // added state variable to track if form has been submitted
 
-    const userCollection = collection(db, "contactform-pokemon")
+    const userCollection = collection(db, "contactform-pokemon");
 
-    const handleSubmit = () => {
-        addDoc(userCollection, {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await addDoc(userCollection, {
             FullName: name,
             Email: email,
             Contact: contactnum,
             Message: feedback,
-        })
+        });
+        setFormSubmitted(true);
     }
+
 
     return (
         <div>
@@ -43,44 +44,67 @@ const Contact = () => {
                     <div className="container contactMargin">
                         <div className="row contactCol1 d-flex align-items-center justify-content-center">
                             <div className="col-6 text-white">
-                                <div className="text-center text-warning pb-5">
-                                    <h1 className="aboutUsHeader">Drop us a line</h1>
-                                </div>
+                                {formSubmitted ? (
+                                    <div className="text-center text-success pb-5">
+                                        <h1 className="aboutUsHeader">Thank you for your message!</h1>
+                                    </div>
+                                ) : (
+                                    <div className="text-center text-warning pb-5">
+                                        <h1 className="aboutUsHeader">Drop us a line</h1>
+                                    </div>
+                                )}
                                 <Form>
                                     <Form.Group className="mb-3" controlId="clickMe1">
                                         <Form.Label className="aboutUsHeader">Full name</Form.Label>
-                                        <Form.Control type="text" placeholder="Juan Dela Cruz" className="hero1SearchButton rounded-5" onChange={(event) => {
-                                            setName(event.target.value)
-                                        }} />
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Juan Dela Cruz"
+                                            className="hero1SearchButton rounded-5"
+                                            onChange={(event) => {
+                                                setName(event.target.value)
+                                            }}
+                                        />
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="clickMe2">
                                         <Form.Label className="aboutUsHeader">Email</Form.Label>
-                                        <Form.Control type="text" placeholder="name@example.com" className="hero1SearchButton rounded-5" onChange={(event) => {
-                                            setEmail(event.target.value)
-                                        }} />
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="name@example.com"
+                                            className="hero1SearchButton rounded-5"
+                                            onChange={(event) => {
+                                                setEmail(event.target.value)
+                                            }}
+                                        />
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="clickMe3">
                                         <Form.Label className="aboutUsHeader">Contact Number</Form.Label>
-                                        <Form.Control type="number" placeholder="09123456789" className="hero1SearchButton rounded-5" onChange={(event) => {
-                                            setContact(event.target.value)
-                                        }} />
+                                        <Form.Control
+                                            type="number"
+                                            placeholder="09123456789"
+                                            className="hero1SearchButton rounded-5"
+                                            onChange={(event) => {
+                                                setContact(event.target.value)
+                                            }}
+                                        />
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="clickMe4">
                                         <Form.Label className="aboutUsHeader">Message</Form.Label>
-                                        <Form.Control className="hero1SearchButton" as="textarea" rows={5} onChange={(event) => {
-                                            setFeedback(event.target.value)
-                                        }} placeholder="Hi! Good Day, I would like to..." />
+                                        <Form.Control
+                                            className="hero1SearchButton"
+                                            as="textarea"
+                                            rows={5}
+                                            onChange={(event) => {
+                                                setFeedback(event.target.value)
+                                            }}
+                                            placeholder="Hi! Good Day, I would like to..."
+                                        />
                                     </Form.Group>
 
                                     <Button className='rounded-5' type="submit" onClick={handleSubmit}>
                                         <img className='hero1Pokeball' src="pokeball.png" alt="" />
-                                        Submit
+                                        Send
                                     </Button>
-
                                 </Form>
-                            </div>
-                            <div className="col-6">
-                                <img src="contactlogo.png" alt="" />
                             </div>
                         </div>
                     </div>
@@ -88,6 +112,7 @@ const Contact = () => {
             </div>
         </div>
     );
-}
+};
 
 export default Contact;
+
