4673d4af440555fbb2f5e5258f51032d720b5919
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-01-03 09:18:14 +0800

    added aos

--- src/components/reusable/Hero1.jsx
+++ src/components/reusable/Hero1.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
         <Row>
           <Col>
 
-            <img src="hero1gcta.png" alt="Pokemon Logo" />
+            <img src="hero1gcta.png" data-aos="zoom-in-down" data-aos-duration="2000" alt="Pokemon Logo" />
 
           </Col>
         </Row>
@@ -43,10 +43,16 @@ const Hero = () => {
                   placeholder="Search by Pokemon ID or Name..."
                   value={search}
                   onChange={(event) => setSearch(event.target.value)}
-                />
+                  data-aos="fade-right"
+                  data-aos-duration="2000"
+                  />
               </Form.Group>
-              <Button className='hero1SearchButton rounded-5' type="submit" onClick={() => setShow(true)} >
-                <img className='hero1Pokeball' src="pokeball.png" alt="" />
+              <Button className='hero1SearchButton rounded-5' type="submit" onClick={() => setShow(true)} 
+              data-aos="fade-left"
+              data-aos-duration="2000"
+              >
+                <img className='hero1Pokeball' src="pokeball.png" alt="" 
+                />
                 Search
               </Button>
             </Form>
