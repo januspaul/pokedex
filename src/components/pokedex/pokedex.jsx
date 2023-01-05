6bab1c4a99ac57919a4dd8f9c84cc0c17f3e11f9
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-01-06 01:05:08 +0800

    Muntik ko nang makuha si filter by type

--- src/components/pokedex/pokedex.jsx
+++ src/components/pokedex/pokedex.jsx
@@ -3,8 +3,6 @@ import axios from "axios";
 import { Button } from "@mui/material";
 import { Container } from "react-bootstrap";
 import PokemonCard from './card';
-import PokemonType from "./type";
-
 
 const SORT_OPTIONS = {
   NUMBER_DESC: 'NUMBER_DESC',
@@ -13,16 +11,13 @@ const SORT_OPTIONS = {
   NAME_DESC: 'NAME_DESC',
 };
 
-
-
 const PokemonCards = () => {
   const [pokemons, setPokemons] = useState([]);
   const [sortOption, setSortOption] = useState(SORT_OPTIONS.NUMBER_ASC);
   const [limit, setLimit] = useState(12);
   const [searchTerm, setSearchTerm] = useState('');
-
-
-
+  const [type, setType] = useState('');
+  const [types, setTypes] = useState([]);
 
   useEffect(() => {
     async function getData() {
@@ -30,11 +25,20 @@ const PokemonCards = () => {
         `https://pokeapi.co/api/v2/pokemon?limit=` + limit
       );
       setPokemons(response.data.results);
-
     }
 
     getData();
   }, [limit]);
+
+  useEffect(() => {
+    async function getTypes() {
+      const response = await axios.get(
+        'https://pokeapi.co/api/v2/type'
+      );
+      setTypes(response.data.results);
+    }
+    getTypes();
+  }, []);
 
   const loadMore = () => {
     setLimit(limit + 12)
@@ -44,34 +48,28 @@ const PokemonCards = () => {
     setSortOption(event.target.value);
   };
 
+  const sortedPokemon = pokemons.sort((a, b) => {
+    switch (sortOption) {
+      case SORT_OPTIONS.NUMBER_ASC:
+        return a.url.match(/\/(\d+)\//)[1] - b.url.match(/\/(\d+)\//)[1];
+      case SORT_OPTIONS.NUMBER_DESC:
+        return b.url.match(/\/(\d+)\//)[1] - a.url.match(/\/(\d+)\//)[1];
+      case SORT_OPTIONS.NAME_ASC:
+        return a.name.localeCompare(b.name);
+      case SORT_OPTIONS.NAME_DESC:
+        return b.name.localeCompare(a.name);
+      default:
+        return 0;
+    }
+  });
 
-
- const sortedPokemon = pokemons.sort((a, b) => {
-  switch (sortOption) {
-    case SORT_OPTIONS.NUMBER_ASC:
-      return a.url.match(/\/(\d+)\//)[1] - b.url.match(/\/(\d+)\//)[1];
-    case SORT_OPTIONS.NUMBER_DESC:
-      return b.url.match(/\/(\d+)\//)[1] - a.url.match(/\/(\d+)\//)[1];
-    case SORT_OPTIONS.NAME_ASC:
-      return a.name.localeCompare(b.name);
-    case SORT_OPTIONS.NAME_DESC:
-      return b.name.localeCompare(a.name);
-    default:
-      return 0; 
-  }
-});
-
-  const filteredPokemon = sortedPokemon.filter(pokemon =>
-    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  
-
+  const filteredPokemon = sortedPokemon
+    .filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    .filter(pokemon => type === 'Showall' || (pokemon.types?.some(t => t.type.name === type)));
 
   return (
     <div className="ps-5">
       <Container className="pokedexContainer">
-        
         <input
           type="text"
           placeholder="Search Pokémon"
@@ -91,7 +89,15 @@ const PokemonCards = () => {
           <option value="galar">Galar</option>
         </select>
 
-        <PokemonType/>
+
+
+        <label htmlFor="type-select" className="text-white px-5">Type:</label>
+        <select id="type-select" value={type} onChange={event => setType(event.target.value)}>
+          <option hidden>Choose type</option>
+          <option value="Showall">Show all</option>
+          {types.map(val => <option key={val.name} className="capitalize" value={val.name}>{val.name}</option>)}
+        </select>
+
 
 
 
@@ -113,7 +119,7 @@ const PokemonCards = () => {
             </div>
           ))}
           <div className="d-flex justify-content-center">
-          <Button className="bg-primary rounded-pill text-white mt-5 mb-4" style={{ width: "200px" }} onClick={loadMore}>Load More</Button>
+            <Button className="bg-primary rounded-pill text-white mt-5 mb-4" style={{ width: "200px" }} onClick={loadMore}>Load More</Button>
           </div>
         </div>
       </Container>
