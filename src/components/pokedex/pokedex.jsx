15b5484d18c6bfeb6a72e90f73157ba11d791b7a
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-01-08 22:29:35 +0800

    set default filter

--- src/components/pokedex/pokedex.jsx
+++ src/components/pokedex/pokedex.jsx
@@ -35,6 +35,10 @@ function PokemonCards() {
       setPokemon(filteredPokemon);
     }
     fetchUrls();
+    if (!typeFilter) {
+      setTypeFilter('normal');
+    }
+    
   }, [results, typeFilter]);
 
 
@@ -45,12 +49,25 @@ function PokemonCards() {
         <label htmlFor="type-filter">Filter by type:</label>
         <select id="type-filter" value={typeFilter} onChange={e => setTypeFilter(e.target.value)}>
           <option value="">All</option>
-          <option value="grass">Grass</option>
-          <option value="fire">Fire</option>
-          <option value="water">Water</option>
+          <option value="bug">Bug</option>
+          <option value="dark">Dark</option>
+          <option value="dragon">Dragon</option>
           <option value="electric">Electric</option>
+          <option value="fairy">Fairy</option>
+          <option value="fighting">Fighting</option>
+          <option value="fire">Fire</option>
+          <option value="flying">Flying</option>
+          <option value="ghost">Ghost</option>
+          <option value="grass">Grass</option>
           <option value="ground">Ground</option>
+          <option value="ice">Ice</option>
+          <option value="normal">Normal</option>
+          <option value="poison">Poison</option>
+          <option value="psychic">Psychic</option>
           <option value="rock">Rock</option>
+          <option value="shadow">Shadow</option>
+          <option value="steel">Steel</option>
+          <option value="water">Water</option>
         </select>
         <div className="row">
           {pokemon
