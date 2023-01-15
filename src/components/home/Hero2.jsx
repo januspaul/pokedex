b5389088182317299cae25eb8acbaa08077fe9be
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-01-15 22:13:17 +0800

    fixed hero2

--- src/components/home/Hero2.jsx
+++ src/components/home/Hero2.jsx
@@ -12,31 +12,31 @@ const Hero2 = () => {
 
     return (
         
-        <div className="py-5">
+        <div className="hero2extend">
             <div className="container">
                <div className="row">
                 <div className="col-12">
             {pokemons.length > 0 && (
                 <div className="row text-white d-flex align-items-center justify-content-center p-2">
-                    <div className="col-md-6 bg-primary rounded-circle hero2img text-center" data-aos="zoom-in" data-aos-duration="1000">
+                    <div className="col-md-6 hero2img text-center" data-aos="zoom-in" data-aos-duration="2000">
                         <img className='img-fluid d-block w-100' src={pokemons[0].sprites.other['official-artwork'].front_default} alt={pokemons[0].name} />
                     </div>
                     <div className="col-md-6 align-middle">
-                        <h1 className='text-center text-warning fst-italic text-uppercase fw-bolder mt-5' data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1000">{pokemons[0].name}</h1>
+                        <h1 className='text-center text-warning fst-italic text-uppercase fw-bolder mt-5' data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2100">{pokemons[0].name}</h1>
                         <div className='display-6'>
                             <table className='table table-borderless text-white'>
                                 <tbody>
-                                    <tr data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1100">
+                                    <tr data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2200">
                                         <th scope="row"></th>
                                         <td className='text-warning fs-2' >Height: </td>
                                         <td className='fs-2'>{pokemons[0].height} m</td>
                                     </tr>
-                                    <tr data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1200">
+                                    <tr data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2300">
                                         <th scope="row"></th>
                                         <td className='text-warning fs-2'>Weight: </td>
                                         <td className='fs-2'>{pokemons[0].weight} kg</td>
                                     </tr>
-                                    <tr data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1300">
+                                    <tr data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2400">
                                         <th scope="row"></th>
                                         <td className='text-warning fs-2'>Type: </td>
                                         <td className='text-capitalize fs-2'><ul>{pokemons[0].types.map((type) => (
@@ -44,7 +44,7 @@ const Hero2 = () => {
                                         ))}
                                         </ul></td>
                                     </tr>
-                                    <tr data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1400">
+                                    <tr data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2500">
                                         <th scope="row"></th>
                                         <td className='text-warning fs-2'>Abilities: </td>
                                         <td className='text-capitalize fs-2'><ul>{pokemons[0].abilities.map((ability) => (
