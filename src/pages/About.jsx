9c692f4c0a966f67108d190f3a0a57e5e81f4adb
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-01-16 09:35:57 +0800

    added animation about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,13 +4,13 @@ const About = () => {
         <div className="BG">
             <div className="container">
                 <div className="row aboutCol d-flex align-items-center justify-content-center">
-                    <div className="col-6 text-white">
+                    <div className="col-6 text-white " data-aos="fade-up" data-aos-duration="2000">
                         <h1 className="aboutUsHeader pb-3 text-warning">About us</h1>
                         <p className="aboutUsDetails">The Pokémon Company International manages the property outside of Asia
                             and is responsible for brand management, licensing, marketing, the Pokémon Trading Card Game,
                             the animated TV series, home entertainment, and the official Pokémon website.</p>
                     </div>
-                    <div className="col-6">
+                    <div className="col-6 text-center" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
                         <img src="abouthero1.png" alt="" />
                     </div>
                 </div>
@@ -22,7 +22,7 @@ const About = () => {
 
             <div className="container d-flex align-items-center justify-content-center pb-4">
                 <div className="row">
-                    <div className="col-4">
+                    <div className="col-4 " data-aos="flip-left"  data-aos-duration="1000">
                         <div className="aboutCard1">
                             <div>
                                 <h2 className="aboutCardTitle px-3 py-2">Passion for Pokémon</h2>
@@ -30,7 +30,7 @@ const About = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-4">
+                    <div className="col-4" data-aos="flip-left" data-aos-duration="2000">
                         <div className="aboutCard2">
                             <div>
                                 <h2 className="aboutCardTitle px-3 py-2">Dedicated to Quality</h2>
@@ -38,8 +38,8 @@ const About = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-4">
-                        <div className="aboutCard3">
+                    <div className="col-4" data-aos="flip-left" data-aos-duration="3000">
+                        <div className="aboutCard3 ">
                             <div>
                                 <h2 className="aboutCardTitle px-3 py-2">Challenging the Expected</h2>
                                 <p className="aboutCardText fs-5 px-3 py-2">We display curiosity and creativity and use these qualities to develop innovative ideas, solve complex problems, and identify unique opportunities.</p>
@@ -51,7 +51,7 @@ const About = () => {
             </div>
             <div className="container d-flex align-items-center justify-content-center">
                 <div className="row">
-                    <div className="col-4">
+                    <div className="col-4" data-aos="flip-left" data-aos-duration="1000">
                         <div className="aboutCard4">
                             <div>
                                 <h2 className="aboutCardTitle px-3 py-2">Building Relationships</h2>
@@ -59,7 +59,7 @@ const About = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-4">
+                    <div className="col-4" data-aos="flip-left" data-aos-duration="2000">
                         <div className="aboutCard5">
                             <div>
                                 <h2 className="aboutCardTitle px-3 py-2">Integrity and Respect</h2>
@@ -67,7 +67,7 @@ const About = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-4">
+                    <div className="col-4" data-aos="flip-left" data-aos-duration="3000">
                         <div className="aboutCard6">
                             <div>
                                 <h2 className="aboutCardTitle px-3 py-2">Delighting Customers</h2>
@@ -84,7 +84,7 @@ const About = () => {
 
             <div className="container d-flex align-items-center justify-content-center pb-5">
                 <div className="row">
-                    <div className="col-3">
+                    <div className="col-3" data-aos="fade-up" data-aos-duration="1000">
                         <a href="https://www.nintendo.com/" target="_blank" rel="noreferrer">
                             <div className="bgCardHoverAbout">
                                 <div className="aboutPartnersCol bg-warning d-flex align-items-center justify-content-center">
@@ -94,7 +94,7 @@ const About = () => {
                             </div>
                         </a>
                     </div>
-                    <div className="col-3">
+                    <div className="col-3" data-aos="fade-up" data-aos-duration="1250">
                         <a href="https://www.gamefreak.co.jp/" target="_blank" rel="noreferrer">
                             <div className="bgCardHoverAbout">
                                 <div className="aboutPartnersCol bg-warning d-flex align-items-center justify-content-center">
@@ -104,7 +104,7 @@ const About = () => {
                             </div>
                         </a>
                     </div>
-                    <div className="col-3">
+                    <div className="col-3" data-aos="fade-up" data-aos-duration="1500">
                         <a href="https://www.creatures.co.jp/" target="_blank" rel="noreferrer">
                             <div className="bgCardHoverAbout">
                                 <div className="aboutPartnersCol bg-warning d-flex align-items-center justify-content-center">
@@ -115,7 +115,7 @@ const About = () => {
                             </div>
                         </a>
                     </div>
-                    <div className="col-3">
+                    <div className="col-3" data-aos="fade-up" data-aos-duration="1750">
                         <a href="https://nianticlabs.com/" target="_blank" rel="noreferrer">
                             <div className="bgCardHoverAbout">
                                 <div className="aboutPartnersCol bg-warning d-flex align-items-center justify-content-center">
